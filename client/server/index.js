b2f4dbbe709bfee67ab9f523317eeda180920535
Veda Ashok <vashok061@gmail.com>
2021-01-31 15:07:36 -0800

    ESlint and prettier setup

--- client/server/index.js
+++ client/server/index.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const socket = require("socket.io");
-const http = require("http");
+const express = require('express')
+const socket = require('socket.io')
+const http = require('http')
 
-const app = express();
-const server = http.createServer(app);
+const app = express()
+const server = http.createServer(app)
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
     // allowedHeaders: ["my-custom-header"],
     // credentials: true
-  }})
+  },
+})
 
-io.on("connection", onConnection);
-
-function onConnection(socket) {
-  console.log("hello");
-  socket.on("drawing", (data) => {
-    console.log(data)
-    socket.broadcast.emit("drawing", data);
-  });
+function onConnection() {
+  console.log('hello')
+  socket.on('drawing', (data) => {
+    socket.broadcast.emit('drawing', data)
+  })
 }
 
-const port = 8080;
-server.listen(port, () => console.log(`server is running on port ${port}`));
+io.on('connection', onConnection)
+
+const port = 8080
+server.listen(port, () => console.log(`server is running on port ${port}`))
