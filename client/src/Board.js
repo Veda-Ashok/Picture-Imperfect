0737329e19aee9911f4edd6098af75bfaef1323a
Bridget <bridgetoconnor808@gmail.com>
2021-01-31 10:48:56 -1000

    shared board working, got rid of colors

--- client/src/Board.js
+++ client/src/Board.js
@@ -1,11 +1,8 @@
 import React, { useRef, useEffect } from 'react';
 import io from 'socket.io-client';
-import './styles/board.css';
-
 
 const Board = () => {
   const canvasRef = useRef(null);
-  const colorsRef = useRef(null);
   const socketRef = useRef();
 
   useEffect(() => {
@@ -13,38 +10,24 @@ const Board = () => {
     // --------------- getContext() method returns a drawing context on the canvas-----
 
     const canvas = canvasRef.current;
-    const test = colorsRef.current;
     const context = canvas.getContext('2d');
 
     // ----------------------- Colors --------------------------------------------------
 
-    const colors = document.getElementsByClassName('color');
-    console.log(colors, 'the colors');
-    console.log(test);
     // set the current color
     const current = {
       color: 'black',
     };
 
-    // helper that will update the current color
-    const onColorUpdate = (e) => {
-      current.color = e.target.className.split(' ')[1];
-    };
-
-    // loop through the color elements and add the click event listeners
-    for (let i = 0; i < colors.length; i++) {
-      colors[i].addEventListener('click', onColorUpdate, false);
-    }
     let drawing = false;
 
     // ------------------------------- create the drawing ----------------------------
 
-    const drawLine = (x0, y0, x1, y1, color, emit) => {
+    const drawLine = (x0, y0, x1, y1, emit) => {
       context.beginPath();
       context.moveTo(x0, y0);
       context.lineTo(x1, y1);
-      context.strokeStyle = color;
-      context.lineWidth = 2;
+      context.lineWidth = 3;
       context.stroke();
       context.closePath();
 
@@ -57,7 +40,6 @@ const Board = () => {
         y0: y0 / h,
         x1: x1 / w,
         y1: y1 / h,
-        color,
       });
     };
 
@@ -71,7 +53,7 @@ const Board = () => {
 
     const onMouseMove = (e) => {
       if (!drawing) { return; }
-      drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true);
+      drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, true);
       current.x = e.clientX || e.touches[0].clientX;
       current.y = e.clientY || e.touches[0].clientY;
     };
@@ -79,7 +61,7 @@ const Board = () => {
     const onMouseUp = (e) => {
       if (!drawing) { return; }
       drawing = false;
-      drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true);
+      drawLine(current.x, current.y, e.clientX || e.changedTouches[0].clientX, e.clientY || e.changedTouches[0].clientY, true);
     };
 
     // ----------- limit the number of events per second -----------------------
@@ -135,14 +117,6 @@ const Board = () => {
   return (
     <div>
       <canvas ref={canvasRef} className="whiteboard" />
-
-      <div ref={colorsRef} className="colors">
-        <div className="color black" />
-        <div className="color red" />
-        <div className="color green" />
-        <div className="color blue" />
-        <div className="color yellow" />
-      </div>
     </div>
   );
 };
