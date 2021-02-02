2ff98b418e5063a2e02cd1aa6ef26d149ec87787
Bridget <bridgetoconnor808@gmail.com>
2021-02-02 13:42:00 -1000

    making prettier format react code on save

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Board from './Board'
+import HomePage from './components/home/HomePage'
 
 export default function App() {
-  return <Board />
+  return <HomePage />
 }
