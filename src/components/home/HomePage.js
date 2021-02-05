5994292a5b24586b3ae0bf6c99d59809c06ae901
Bridget <bridgetoconnor808@gmail.com>
2021-02-05 12:27:31 -1000

    adding routes

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
 
 export default function HomePage() {
-  return <h1>Picture Imperfect</h1>
+  return (
+    <>
+      <h1>Picture Imperfect</h1>
+      <Button>Create Game</Button>
+      <Button>Join Game</Button>
+    </>
+  )
 }
