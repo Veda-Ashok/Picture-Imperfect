be6d40c00b4b7ae466506c226e4cf31947396d82
Bridget <bridgetoconnor808@gmail.com>
2021-02-11 00:48:45 -1000

    created mock context to pass the absolute base case for homepage

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,6 +1,7 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import Button from '@material-ui/core/Button'
-// import Form from '@material-ui/core/Form'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
 import TextField from '@material-ui/core/TextField'
 import { useHistory } from 'react-router-dom'
 import io from 'socket.io-client'
@@ -12,88 +13,117 @@ export default function HomePage() {
   const globalContext = useContext(Context)
   const history = useHistory()
   const [roomCode, setRoomCode] = useState(undefined)
+  const [username, setUsername] = useState(undefined)
+  const [command, setCommand] = useState(undefined)
+  const [usernameError, setUsernameError] = useState('')
+  const [customWords, setCustomWords] = useState(false)
   const [openCreateGame, setOpenCreateGame] = useState(false)
   const [openJoinGame, setOpenJoinGame] = useState(false)
+  const [openUsernameError, setOpenUsernameError] = useState(false)
 
+  // if they are in a room right now remove them from the room
+  useEffect(() => {
+    if (globalContext.roomCode) {
+      globalContext.addRoomCode(undefined, globalContext)
+      globalContext.socket.emit('disconnect')
+    }
+  }, [])
+
+  // Room handlers
   const handleRoomCode = (e) => {
     console.log(roomCode)
     setRoomCode(e.target.value)
   }
 
   const handleCreateGameOpen = () => {
+    setCommand('createRoom')
     setOpenCreateGame(true)
   }
 
-  const handleJoinGameOpen = () => {
-    setOpenJoinGame(true)
-  }
-
   const handleCreateGameClose = () => {
+    setCommand('')
     setOpenCreateGame(false)
   }
 
+  const handleJoinGameOpen = () => {
+    setCommand('joinRoom')
+    setOpenJoinGame(true)
+  }
+
   const handleJoinGameClose = () => {
+    setCommand('')
     setOpenJoinGame(false)
   }
 
-  const handleJoin = async (event) => {
+  // Username handlers
+  const handleUsername = (e) => {
+    console.log('username', username)
+    setUsername(e.target.value)
+  }
+
+  const handleUsernameErrorOpen = () => {
+    setUsername(undefined)
+    setOpenUsernameError(true)
+  }
+
+  const handleUsernameErrorClose = () => {
+    setOpenUsernameError(false)
+  }
+
+  const goToRoom = async (event) => {
     event.preventDefault()
-    try {
-      const socket = io.connect('ws://localhost:8080')
-      console.log('made socket')
-      socket.emit('joinRoom', { username: 'Lord Slug', room: roomCode })
-      console.log('joined room?')
-      // need to check if the roomcode exists
-      globalContext.addRoomCode(roomCode, globalContext)
-      globalContext.addSocket(socket, globalContext)
-      console.log('socket stuff over')
-      history.push('/lobby')
-    } catch (error) {
-      console.log(error)
+    if (username === undefined) {
+      handleUsernameErrorOpen()
+      setUsernameError('Please enter a username')
+    } else {
+      try {
+        let room
+        let socket
+        const usernameTaken = true
+        if (!globalContext.socket) {
+          socket = io.connect('ws://localhost:8080')
+          console.log('made socket')
+          globalContext.addSocket(socket, globalContext)
+        } else {
+          socket = globalContext.socket
+        }
+        // we need a socket command to check username
+        // usernameTaken = data.usernameTaken
+        if (usernameTaken) {
+          handleUsernameErrorOpen()
+          setUsernameError('Sorry that username is taken')
+        } else {
+          if (command === 'createRoom') {
+            socket.emit(command, { username, customWords })
+          } else if (command === 'joinRoom') {
+            socket.emit('joinRoom', { username, room: roomCode })
+          }
+          await new Promise((resolve) => {
+            socket.on('roomUsers', async (data) => {
+              console.log('onRoomUsers data.room', data.room)
+              room = data.room
+              resolve(data)
+            })
+          })
+
+          globalContext.addRoomCode(room, globalContext)
+          history.push('/lobby')
+        }
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 
-  // const onRoomUsers = (data) => {
-  //   console.log('onRoomUsers', data)
-  //   console.log('onRoomUsers data.room', data.room)
-  //   setRoomCode(data.room)
-  //   // data.users
-  // }
-
-  const handleCreate = async (event) => {
-    event.preventDefault()
-    try {
-      let room
-      const socket = io.connect('ws://localhost:8080')
-      console.log('made socket')
-      // TODO: Create room function
-      socket.emit('createRoom', { username: 'Creation Slug', customWords: true })
-
-      await new Promise((resolve) => {
-        socket.on('roomUsers', async (data) => {
-          console.log('onRoomUsers', data)
-          console.log('onRoomUsers data.room', data.room)
-          room = data.room
-          resolve(data)
-        })
-      })
-      console.log('room code before global state: ', room)
-
-      globalContext.addRoomCode(room, globalContext)
-      console.log('created room?')
-      globalContext.addSocket(socket, globalContext)
-      console.log('socket stuff over')
-      console.log('room code after global state: ', room)
-
-      history.push('/lobby')
-    } catch (error) {
-      console.log(error)
-    }
+  const handleCustomWords = (event) => {
+    setCustomWords(event.target.checked)
   }
 
   return (
     <div>
       <h1>Picture Imperfect</h1>
+
+      <img src="/logo192.png" alt="logo" />
 
       <Button variant="contained" color="secondary" onClick={handleCreateGameOpen}>
         Create Game
@@ -103,25 +133,57 @@ export default function HomePage() {
         Join Game
       </Button>
 
+      <Dialog
+        onClose={handleUsernameErrorClose}
+        aria-labelledby="username-error"
+        open={openUsernameError}
+      >
+        <DialogTitle>{usernameError}</DialogTitle>
+      </Dialog>
+
       <Dialog onClose={handleCreateGameClose} aria-labelledby="create-game" open={openCreateGame}>
-        <form onSubmit={handleCreate}>
-          <DialogTitle id="create-game-title">Create Game</DialogTitle>
-          <Button variant="contained" color="secondary" type="submit" onClick={handleCreate}>
+        <DialogTitle id="create-game-title">Create Game</DialogTitle>
+        <form onSubmit={goToRoom}>
+          <TextField
+            id="outlined-basic"
+            label="Enter username"
+            variant="outlined"
+            onChange={(e) => handleUsername(e)}
+          />
+          <FormControlLabel
+            /* eslint-disable */
+            control={
+              <Checkbox
+                checked={customWords}
+                onChange={handleCustomWords}
+                name="custom-words-checkbox"
+              />
+            }
+            /* eslint-disable */
+            label="Add your own custom words?"
+          />
+          <Button variant="contained" color="secondary" type="submit">
             Create Game
           </Button>
         </form>
       </Dialog>
 
-      <Dialog onClose={handleJoinGameClose} aria-labelledby="create-game" open={openJoinGame}>
-        <form onSubmit={handleJoin}>
-          <DialogTitle id="create-game-title">Join Game</DialogTitle>
+      <Dialog onClose={handleJoinGameClose} aria-labelledby="join-game" open={openJoinGame}>
+        <DialogTitle id="create-game-title">Join Game</DialogTitle>
+        <form onSubmit={goToRoom}>
           <TextField
             id="outlined-basic"
             label="Enter room code"
             variant="outlined"
             onChange={(e) => handleRoomCode(e)}
           />
-          <Button variant="contained" color="secondary" type="submit" onClick={handleJoin}>
+          <TextField
+            id="outlined-basic"
+            label="Enter username"
+            variant="outlined"
+            onChange={(e) => handleUsername(e)}
+          />
+          <Button variant="contained" color="secondary" type="submit">
             Join Game
           </Button>
         </form>
