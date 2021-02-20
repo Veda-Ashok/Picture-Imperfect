97d36b3274115117c11a96697f80b46edcfc5db1
ktachiyama <k_tachiyama@yahoo.com>
2021-02-19 23:36:13 -0800

    pls

--- server/index.js
+++ server/index.js
@@ -124,4 +124,4 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log(`server is running on port ${PORT}`))
 
-// export default PORT
+export default PORT
