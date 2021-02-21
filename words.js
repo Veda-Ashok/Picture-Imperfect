133a53f0b7b3577d97ca4399889151bfd43aa4bc
Veda Ashok <vashok061@gmail.com>
2021-02-21 13:25:30 -0800

    Words

--- words.js
+++ words.js
@@ -4,4 +4,4 @@
 const easy = ['cat', 'dog', 'umbrella', 'mouse', 'apple', 'banana', 'pineapple']
 const medium = ['merry-go-round', 'polaroid', 'photo album']
 const difficult = ['high school', 'carnival', 'castle']
-const impossible = ['mystery', 'orange']
+const impossible = ['mystery', 'orange', 'novel', 'regret', 'excited']
