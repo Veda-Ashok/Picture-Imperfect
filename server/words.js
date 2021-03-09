06defe4e56d55047a2fd7f811e2c1165e0779eb2
Rachel Nguyen <rnguye12@lion.lmu.edu>
2021-03-08 20:42:41 -0600

    added custom word functionality where there's 1/5 chance getting custom

--- server/words.js
+++ server/words.js
@@ -1,12 +1,33 @@
 /* eslint-disable no-unused-vars */
 /* Words for game assignemnt by level */
 
+// end of game, clear custom words
+// what do if there's only 1 custom word? do we pick from a different difficulty, or require at least 2 custom words?
+
 const easy = ['cat', 'dog', 'umbrella', 'mouse', 'apple', 'banana', 'pineapple']
 const medium = ['merry-go-round', 'polaroid', 'photo album']
 const hard = ['high school', 'carnival', 'castle']
 const impossible = ['mystery', 'orange', 'novel', 'regret', 'excited']
+const custom = {}
 
-const words = { easy, medium, hard, impossible }
+const words = { easy, medium, hard, impossible, custom }
+
+function addCustomWord(roomCode, word) {
+  if (Object.prototype.hasOwnProperty.call(custom, roomCode)) {
+    if (!custom[roomCode].find((current) => current === word)) {
+      custom[roomCode] = [...custom[roomCode], word]
+    }
+    console.log('if room already exists', Object.values(custom))
+  } else {
+    custom[roomCode] = [word]
+    console.log('room doesnt exists', Object.values(custom))
+  }
+}
+
+function clearCustomWords(roomCode) {
+  delete custom[roomCode]
+  console.log(custom[roomCode])
+}
 
 function getRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
@@ -16,12 +37,20 @@ function getRandomDifficulty() {
   return getRandom(Object.keys(words))
 }
 
-function getRandomWord(difficulty) {
-  return getRandom(words[difficulty])
+function getRandomWord(difficulty, roomCode) {
+  let wordList
+  if (difficulty === 'custom') {
+    wordList = custom[roomCode]
+  } else {
+    wordList = words[difficulty]
+  }
+  return getRandom(wordList)
 }
 
 module.exports = {
   words,
+  addCustomWord,
+  clearCustomWords,
   getRandomDifficulty,
   getRandomWord,
 }
