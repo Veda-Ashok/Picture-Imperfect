3137576f4f93065316565c53f13ce1e43d6efbed
Rachel Nguyen <rnguye12@lion.lmu.edu>
2021-04-11 20:59:31 -0500

    pls

--- server/index.js
+++ server/index.js
@@ -290,7 +290,7 @@ io.on('connection', (socket) => {
     // console.log('total Rounds: ', totalRounds)
   })
 })
-
+// pls
 // const PORT = process.env.PORT || 8080 // comment this to run locally
 const PORT = 8080 // then uncomment this
 http.listen(PORT, () => console.log(`server is running on port ${PORT}`))
